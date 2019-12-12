65268b308bdc063c76cb99ea02f1b9ecbe366a16
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-12-12 13:53:46 +0100

    added so that you get json file with all the stops

--- build/server/routes/routes.js
+++ build/server/routes/routes.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const authenticationToken_1 = require("../authenticationToken");
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const fs_1 = __importDefault(require("fs"));
 let router = express_1.default.Router();
 router.get('/', authenticationToken_1.authentication);
 /* router.get('/', (req: Request, res: Response, next: NextFunction) => {
@@ -15,14 +16,25 @@ router.get('/', authenticationToken_1.authentication);
 router.get('/', (req, res, next) => {
     let bearer = authenticationToken_1.token.tokenType;
     let accessToken = authenticationToken_1.token.accessToken;
-    axios_1.default.get('https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=ols&format=json', {
+    axios_1.default.get('https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json', {
         headers: {
             Authorization: bearer + " " + accessToken
         }
     })
         .then(function (response) {
-        res.status(200).json({ response: response.data });
+        res.status(200).json(response.data);
         console.log(response.data);
+        fs_1.default.readFile('./All_stops.json', 'utf-8', function (err, data) {
+            if (err)
+                throw err;
+            var arrayOfObjects = JSON.parse(data);
+            arrayOfObjects.All_Stops.push(response.data.LocationList);
+            fs_1.default.writeFile('./All_stops.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
+                if (err)
+                    throw err;
+                console.log("done");
+            });
+        });
     }).catch(function (error) {
         console.log("ERROR: ", error);
     }).then(next);
