fa3d7db6f82dec2a10866bde3a5e8aa537682dae
Ranchino <ranjbahadin@live.se>
2019-12-13 12:56:56 +0100

    testing function

--- src/client/script/script.ts
+++ src/client/script/script.ts
@@ -1,1 +1,40 @@
-console.log("Script")
+console.log("Script");
+
+
+/* (function () {
+    function switchInputText(){
+        console.log("Inne i funktionen");
+        let inputFrom = (<HTMLInputElement>document.getElementById("inputFrom"));
+        let inputTo = (<HTMLInputElement>document.getElementById("inputTo"));
+
+        if(inputFrom.value == "" || inputTo.value == ""){
+            alert("fyll in alla fält");
+        }else{
+            inputFrom.innerHTML = inputTo.value;
+            inputTo.innerHTML = inputFrom.value;
+        }
+        
+    }
+
+    (<HTMLInputElement>document.getElementById('switchInputText')).addEventListener('click', switchInputText, true);
+})(); */
+
+class inputChange {
+    constructor() {
+
+        (<HTMLInputElement>document.getElementById("switchInputText")).addEventListener("click", (e:Event) => this.switchInputText());
+    }
+    switchInputText(){
+        let inputFrom = (<HTMLInputElement>document.getElementById("inputFrom"));
+        let inputTo = (<HTMLInputElement>document.getElementById("inputTo"));
+
+        if(inputFrom.value == "" || inputTo.value == ""){
+            alert("fyll in alla fält");
+        }else{
+            inputFrom.innerHTML = inputTo.value;
+            inputTo.innerHTML = inputFrom.value;
+        }
+    }
+}
+
+new inputChange
