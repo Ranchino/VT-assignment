f77a6b4af94854054d967a124910a413ae8855e3
Ranchino <ranjbahadin@live.se>
2019-12-13 12:56:01 +0100

    fixing the form styling

--- copy-deps.js
+++ copy-deps.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const root = './src';
 const outdir = './build';
-const filter = ['.html', '.css', '.png'];
+const filter = ['.html', '.css', '.png', '.ts'];
 const files = [];
 
 function explore(root, filter, output) {
