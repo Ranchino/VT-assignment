f942a0dd28afbc577e3fab316b3d71fdb7bf497c
davidyanis <daveddd97@hotmail.com>
2019-12-14 21:42:17 +0100

    rest api search list

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -2,9 +2,7 @@ import { authentication, token } from '../authenticationToken'
 import express, { Request, Response, NextFunction, response } from 'express'
 import axios from 'axios'
 import fs from 'fs'
-import { json } from 'body-parser'
 let router = express.Router()
-
 
 router.get('/', authentication)
 /* router.get('/', (req: Request, res: Response, next: NextFunction) => {
@@ -20,21 +18,47 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
         }
     })
     .then(function(response){
-        res.status(200).json(response.data)
-        console.log(response.data)
-       fs.readFile('./All_stops.json', 'utf-8', function(err, data){
-           if(err) throw err
-           var arrayOfObjects = JSON.parse(data)
-           arrayOfObjects.All_Stops.push(response.data.LocationList)
-        fs.writeFile('./All_stops.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err){
+        fs.readFile('./All_stops.json', 'utf-8', function(err, data){
             if(err) throw err
-            console.log("done")
+            var arrayOfObjects = JSON.parse(data)
+            arrayOfObjects.All_Stops.push(response.data.LocationList)
+
+            fs.writeFile('./All_stops.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err){
+                if(err) throw err
+                console.log("done")
+            })
         })
-       })
-    
     }).catch(function(error) {
         console.log("ERROR: ", error)
     }).then(next)
+})
+
+router.post('/location', (req: Request, res: Response, next: NextFunction) => {
+    let array: any = [];
+    let searchResults: any;
+    // Resets array for every input liste
+    array = [];
+    fs.readFile('./All_stops.json', 'utf-8', function(err, data){
+        var arrayOfObjects = JSON.parse(data)
+        let allStopsList = arrayOfObjects.All_Stops[0].StopLocation
+        for (var i = 0; i < allStopsList.length; i++) {
+            if (allStopsList[i].name.includes(req.body.searchValue)) {
+                searchResults = {
+                    'hallplats': arrayOfObjects.All_Stops[0].StopLocation[i].name,
+                    'id': arrayOfObjects.All_Stops[0].StopLocation[i].id
+                }
+                
+
+                array.push(searchResults)
+
+                if (array.length === 10) {
+                    break;
+                }
+              
+            }
+        }
+        res.send(array)
+    })
 })
 
 export default router
