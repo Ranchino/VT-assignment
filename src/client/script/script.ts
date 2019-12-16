e61d58ef9f1bb12e10417bd7414cce1fc616cac7
Ranchino <ranjbahadin@live.se>
2019-12-16 13:47:29 +0100

    local time function

--- src/client/script/script.ts
+++ src/client/script/script.ts
@@ -17,4 +17,52 @@ function switchInputText(){
 window.onload=function(){
     
     (<HTMLInputElement>document.getElementById('switchInputText')).addEventListener('click', switchInputText, true);
+    getDate();
+}
+
+// Test
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.body.addEventListener("submit", async function(event) {
+
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement;
+
+        // casting to any here to satisfy tsc
+        // sending body as x-www-form-url-encoded
+        const result = await fetch(form.action, {
+        method: form.method,
+        body: new URLSearchParams([...(new FormData(form) as any)])
+        })        
+        .then((response: Response) => response.json())
+        .then(json => json)
+        .catch(error => console.log(error));        
+    });
+})
+
+function getDate(){
+    var today: any = new Date();
+    var dd: any = today.getDate();
+    var mm: any = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = yyyy + '-' + mm + '-' + dd;
+    console.log(today);
+    (<HTMLInputElement>document.getElementById("dateForTrip")).value = today;
+
+    var time = hour + ":" + minute;
+    console.log(time);
+    (<HTMLInputElement>document.getElementById("timeForTrip")).value = time;
 }
