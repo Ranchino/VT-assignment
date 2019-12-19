9fadb7993d9411a683b6b7b62b29f93bd219e1f0
Sebastian Johansson <sebben.97@hotmail.com>
2019-12-19 12:28:54 +0100

    JSON file now only refreshes once ever 24 hours.

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -4,37 +4,46 @@ import axios from 'axios'
 import fs from 'fs'
 import { json } from 'body-parser'
 let router = express.Router()
-
+//let today = Date.now()
+let expireTime: number = 0
 
 router.get('/', authentication)
-/* router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ token: token })
-    next()
-}) */
+
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    let bearer: string = token.tokenType
-    let accessToken: string = token.accessToken
-    axios.get('https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json', {
-        headers: {
-            Authorization: bearer + " " + accessToken
-        }
-    })
-    .then(function(response){
-        res.status(200).json(response.data)
-        console.log(response.data)
-       fs.readFile('./All_stops.json', 'utf-8', function(err, data){
-           if(err) throw err
-           var arrayOfObjects = JSON.parse(data)
-           arrayOfObjects.All_Stops.push(response.data.LocationList)
-        fs.writeFile('./All_stops.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err){
-            if(err) throw err
-            console.log("done")
+
+    console.log("Expire: ", expireTime)
+    console.log("Now: ", Date.now())
+
+    if(Date.now() >= expireTime){
+        
+        let bearer: string = token.tokenType
+        let accessToken: string = token.accessToken
+        axios.get('https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json', {
+            headers: {
+                Authorization: bearer + " " + accessToken
+            }
         })
-       })
+        .then(function(response){
+            expireTime = Date.now() + (1000 * 60 * 60 * 24) 
+            res.status(200).json(response.data)
+            fs.readFile('./All_stops.json', 'utf-8', function(err, data){
+            if(err) throw err
+            var arrayOfObjects = JSON.parse(data)
+            arrayOfObjects.All_Stops.push(response.data.LocationList)
+            fs.writeFile('./All_stops.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err){
+                if(err) throw err
+                console.log("done")
+            })
+            })
+        }).catch(function(error) {
+            console.log("ERROR: ", error)
+        }).then(next)
+    }else {
+        next()
+    }
+        /* console.log("asd")
+        next() */
     
-    }).catch(function(error) {
-        console.log("ERROR: ", error)
-    }).then(next)
 })
 
 export default router
